0b2708e5b2958c718fb32dfc57ca853e131ff1e9
jukyung kim <victoryjkkim92@gmail.com>
2018-10-05 22:58:16 +0900

    snake_update

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -17,63 +17,54 @@ function SnakeGameLogic() {
   this.fruit = {x: 3, y: 5};
 }
 
-let memory = ''
+let memory = '';
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
-  this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y - 1 });
-  this.joints.pop();
-  memory += 'up';
+  memory = 'up';
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function () {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
-  this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y + 1 })
-  this.joints.pop()
-  memory += 'down';
+  memory = 'down';
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
-  this.joints.unshift({ x: this.joints[0].x - 1, y: this.joints[0].y });
-  this.joints.pop()
-  memory += 'left';
+  memory = 'left';
   console.log('left');
 }
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
-  this.joints.unshift({ x: this.joints[0].x + 1, y: this.joints[0].y});
-  this.joints.pop()
-  memory += 'right'
+  memory = 'right';
   console.log('right');
 }
 
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
   if (memory === 'up'){
-    this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y + 1 })
+    this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y - 1 })
     this.joints.pop()
   } else if (memory === 'down'){
     this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y + 1 })
     this.joints.pop()
   } else if (memory === 'right'){
-    this.joints.unshift({ x: this.joints[0].x + 1, y: this.joints[0].y });
+    this.joints.unshift({ x: this.joints[0].x + 1, y: this.joints[0].y })
     this.joints.pop()
-  } else (memory === 'left'){
-    this.joints.unshift({ x: this.joints[0].x - 1, y: this.joints[0].y });
+  } else if (memory === 'left') {
+    this.joints.unshift({ x: this.joints[0].x - 1, y: this.joints[0].y })
     this.joints.pop()
   }
-
-
-
   // 게임이 아직 끝나지 않았으면 `true`를 반환
   // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
   return true;
 }
+
+this.joints.push()
 
 export default SnakeGameLogic;
 
