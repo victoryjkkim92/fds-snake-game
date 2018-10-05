75b01bb65dbefe5114a4349836ad33a9e786233f
jukyung kim <victoryjkkim92@gmail.com>
2018-10-05 18:58:03 +0900

    snake update

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -17,10 +17,13 @@ function SnakeGameLogic() {
   this.fruit = {x: 3, y: 5};
 }
 
+let memory = ''
+
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
   this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y - 1 });
   this.joints.pop();
+  memory += 'up';
   console.log('up');
 }
 
@@ -28,6 +31,7 @@ SnakeGameLogic.prototype.down = function () {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
   this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y + 1 })
   this.joints.pop()
+  memory += 'down';
   console.log('down');
 }
 
@@ -35,6 +39,7 @@ SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
   this.joints.unshift({ x: this.joints[0].x - 1, y: this.joints[0].y });
   this.joints.pop()
+  memory += 'left';
   console.log('left');
 }
 
@@ -42,11 +47,28 @@ SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
   this.joints.unshift({ x: this.joints[0].x + 1, y: this.joints[0].y});
   this.joints.pop()
+  memory += 'right'
   console.log('right');
 }
 
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
+  if (memory === 'up'){
+    this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y + 1 })
+    this.joints.pop()
+  } else if (memory === 'down'){
+    this.joints.unshift({ x: this.joints[0].x, y: this.joints[0].y + 1 })
+    this.joints.pop()
+  } else if (memory === 'right'){
+    this.joints.unshift({ x: this.joints[0].x + 1, y: this.joints[0].y });
+    this.joints.pop()
+  } else (memory === 'left'){
+    this.joints.unshift({ x: this.joints[0].x - 1, y: this.joints[0].y });
+    this.joints.pop()
+  }
+
+
+
   // 게임이 아직 끝나지 않았으면 `true`를 반환
   // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
