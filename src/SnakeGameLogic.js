bbb481f55e4c89977c3c1ab508776d9806775e75
jukyung kim <victoryjkkim92@gmail.com>
2018-10-08 15:42:53 +0900

    snake_game_final

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -48,6 +48,7 @@ SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
   let newHead;
   let newFruit;
+
   //  화살표 키를 누르지 않아도 주기적으로 뱀이 움직이도록 만들어보세요. (힌트: 방금 전에 무슨 키를 눌렀더라..?)
   if (this.direction === 'up'){
     newHead = { 
@@ -72,21 +73,22 @@ SnakeGameLogic.prototype.nextState = function() {
   }
 
   //  뱀의 머리가 벽에 부딪혔을 때 게임이 끝나게 만들어보세요.
-  //  뱀의 머리가 자기 몸에 부딪혔을 때 게임이 끝나도록 만들어보세요.
+  // -> 머리의 y좌표가 row(열)이랑 같거나 클때, 0보다 작을때 | 머리의 x가 행보다 크거나 같을때, 0보다 작을때
   if (newHead.y >= ROWS || newHead.y < 0 || newHead.x >= COLS || newHead.x < 0 || 
+    //  뱀의 머리가 자기 몸에 부딪혔을 때 게임이 끝나도록 만들어보세요.
+    // -> 배열 안의 요소들 중에 조건을 만족하는 요소가 있는지 체크해봄 : 화살표 함수 사용
     this.joints.some(joint => joint.x === newHead.x && joint.y === newHead.y)) {
     return false;
   }
   //  먹이를 먹었을 때 뱀의 길이가 늘어나게 만들어보세요. (힌트: 꼬리를 떼지 않으면..?)
   if (!(newHead.x === this.fruit.x && newHead.y === this.fruit.y)) {
-
+      // 먹이의 좌표와 뱀머리의 좌표가 같을 때가 아닐땐 꼬리를 때고!
     this.joints.pop();
   } else {
     //  먹이를 먹었을 때 다른 곳에 먹이가 생성되게 만들어보세요. 
     newFruit = { x: Math.floor(Math.random() * COLS), y: Math.floor(Math.random() * ROWS) };
     this.fruit = newFruit;
   }
-
   this.joints.unshift(newHead);
   
   
@@ -106,4 +108,3 @@ export default SnakeGameLogic;
 
 // 도전 과제: 
 
-//  먹이가 뱀의 몸과 겹쳐져 생성되는 일이 없게 만들어보세요.
